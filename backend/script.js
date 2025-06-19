d577b92cf9c1bc0b0f51534b77cccb67d6f079ca
anandrajeshmishra <anand98425@gmail.com>
2025-06-19 15:15:13 +0530

    work

--- backend/script.js
+++ backend/script.js
@@ -1,4 +1,15 @@
-var hello = require('./script2')
-var a = 24;
-console.log("anandkumar mishra");
-console.log(hello);
+// var hello = require('./script2')
+// var a = 24;
+// console.log("anandkumar mishra");
+// var oneLinerJoke = require('one-liner-joke');
+// var figlet = require("figlet");
+var figlet = require("figlet");
+
+figlet("ANANDKUMAR", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
